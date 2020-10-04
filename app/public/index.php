b2e6a314270af23f564eb65bd1b016e4416d4983
Steven Woolston <steven.woolston@gmail.com>
2020-10-04 22:44:11 +1000

    WIP

---
+++ app/public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'inc/head.php'; ?>
+<body>
+    <?php include 'inc/header.php'; ?>
+    <main id="main">
+    
+    </main>
+    <?php include 'inc/footer.php'; ?>
+</body>
+</html>

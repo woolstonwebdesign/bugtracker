20189e651d07bbd7616e3a5e62b07bcad6d82a1e
Steven Woolston <steven.woolston@gmail.com>
2020-10-06 23:33:15 +1000

    Deployed

--- app/public/index.php
+++ app/public/index.php
@@ -4,50 +4,59 @@
     <main id="main">
         <section class="content">
 <?php
+
+if (empty($_GET['repo'])):
+    echo '<h1 class="text-center">Nothing to show</h1>';
+    die();
+endif;
+
 require_once(__DIR__ . '/client/GitHubClient.php');
-
 $owner = 'woolstonwebdesign';
-$repo = 'veriskills';
-
+$repo = $_GET['repo'];
 $client = new GitHubClient();
-$client->setCredentials('design@woolston.com.au', '<password>');
+$client->setCredentials('design@woolston.com.au', 'HannahN0ah');
 $client->setPage();
 $client->setPageSize(2);
 $repository = $client->repos->get($owner, $repo);
 $issues = $client->issues->listIssues($owner, $repo);
 ?>
-            <article id="issues">
-                <h2>Issues for <?php echo $repository->getDescription(); ?></h2>
-                <table class="table">
-                    <colgroup>
-                        <col style="width: 30px;" />
-                        <col />
-                        <col style="width: 30px;" />
-                    </colgroup>
-                    <tbody>
+            <h2>Active Issues for <?php echo $repository->getDescription(); ?></h2>
 <?php
 foreach ($issues as $issue)
 {
     /* @var $issue GitHubIssue */
     $issue_id = $issue->getNumber();
-    echo '<tr><td>' .$issue_id. '</td><td>' .$issue->getTitle(). '</td><td><a class="btn btn-primary" href="issue.php?i=' .$issue_id. '">View</a></td></tr>';
-    //echo get_class($issue) . "[" . $issue->getNumber() . "]: " . $issue->getTitle() . "\n";
-}
 ?>
-                    </tbody>
-                </table>
-            </article><!-- issues -->
+            <article class="issue">
+                <div class="issue-container">
+                    <div class="issue-title d-flex align-items-center">
+                        <span class="badge badge-secondary mr-3">#<?php echo $issue_id; ?></span>
+                        <h5><?php echo $issue->getTitle(); ?></h5>
+                        <a data-toggle="collapse" href="#issue-comments-<?php echo $issue_id; ?>" 
+                            role="button" aria-expanded="false" aria-controls="collapseExample" >
+                            <i class="las la-plus-circle"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div id="issue-comments-<?php echo $issue_id; ?>" class="collapse">
 <?php
-    foreach ($issues as $issue):
-        $issue_id = $issue->getNumber();
-        $comments = $client->issues->listCommentsOnAnIssue($owner, $repo, $issue_id);
-        var_dump($comments);
+        $comments = $client->issues->comments->listCommentsOnAnIssue($owner, $repo, $issue_id);
+        // var_dump($comments);
+        if (!empty($comments)) {
+            echo '<ul>';
+            foreach ($comments as $comment):
+                echo '<li>' . $comment->getBody();
+            endforeach;
+            echo '</ul>';
+        } else {
+            echo '<div class="text-center">There are no comments</div>';
+        }
 ?>
-            <article data-id="<?php echo $issue_id; ?>">
-                <h2><?php echo $issue->getTitle(); ?></h2>
+                </div>
             </article>
 <?php
-    endforeach;
+}
 ?>
         </section>
     </main>
